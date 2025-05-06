f99daa28a9c525d8c9ff00c289ef08c93e1744b0
Bryan Gordon <bryanbge92@gmail.com>
2025-05-05 21:44:51 -0500

    Update index

--- wordle/index.jsx
+++ wordle/index.jsx
@@ -1,40 +1,105 @@
+import { bold, white, bgGreen, bgYellow, bgBrightBlack } from "jsr:@std/fmt/colors"
+import { isCreative } from './env.js'
+import { writePokemonFile } from './file.js'
+
+const tries = 1
 const MAX_TRIES = 6
 const previousGuesses = []
+const colorMethods = {
+  green: bgGreen,
+  yellow: bgYellow,
+  gray: bgBrightBlack
+}
 
 let globalResults = ''
+const POKEMON_AVAILABLE = 850
+const randomId = Math.ceil(Math.random() * (POKEMON_AVAILABLE - 1))
 
-const pokemon = 'pikachu'
+const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+  .then(res => res.json())
+  .then(response => response.name.toUpperCase())
+
+if (isCreative) {
+  writePokemonFile(pokemon)
+}
 
 function askWord () {
   const typed = prompt('The Pokemon is...')
-  console.log(typed)
 
-  if (typed === undefined) {
+  if (typed == null) {
     return { error: 'You must be write a pokemon name.' }
   } 
   else if (typed.length !== pokemon.length) {
     return { error: 'Pokemon must be have ' + pokemon.length + ' characteres long'}
   } 
-  else if (previousGuesses.includes(typed)) {
+  else if (previousGuesses.includes(typed.toUpperCase())) {
     return { error: 'You already try this pokemon name.'}
   } 
   else if (!/^[a-zA-Z]+$/.test(typed)) {
     return { error: 'The pokemon name must be only letters'}
   }
 
-  return { response: typed}
+  return { typed: typed.toUpperCase() }
 }
 
-let guess = ''
-let tries = 0
+function colorLetters (color, letter) {
+  const bg = colorMethods[color]
+  const styledLetter =  bg(bold(` ${white(letter)} `))
+  return ` ${styledLetter} `
+}
 
-while (guess === '') {
-  const { error, response } = askWord()
+function print (guess) {
+  console.clear()
 
-  if (error) {
-    console.error(error)
-    continue
+  let results = ''
+  const letters = [...guess]
+
+  letters.forEach((letter, index) => {
+    if (letter === pokemon[index]) {
+      results += colorLetters('green', letter)
+    } else if (pokemon.includes(letter)) {
+      results += colorLetters('yellow', letter)
+    } else {
+      results += colorLetters('gray', letter)
+    }
+  })
+
+  globalResults += `${results}\n\n`
+  console.log(globalResults)
+}
+
+function start (tries) {
+  if (tries >= MAX_TRIES) {
+    console.log('You lost')
+    console.log('The pokemon was ' + pokemon)
+    return
   }
 
-  if (response) guess = response
+  let guess = ''
+  while (guess === '') {
+    const { error, typed } = askWord()
+  
+    if (error) {
+      console.error(error)
+      continue
+    }
+  
+    if (typed) guess = typed
+  }
+
+  if (guess === pokemon) {
+    print(guess)
+    console.log('You Won!')
+  } else {
+    print(guess)
+    tries++
+    console.log('')
+    console.log(tries)
+    start(tries)
+  }
+
 }
+
+console.log('Let\'s play a game!! Guess the pokemon name')
+console.log(`Hint: It has ${pokemon.length} characters...`)
+start(tries)
