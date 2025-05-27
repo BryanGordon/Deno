56fd5b46b4f20a2aadf0f94c0bcce93c003cacef
Bryan Gordon <bryanbge92@gmail.com>
2025-05-27 18:29:20 -0500

    Add Boolean converted

--- wordle/env.js
+++ wordle/env.js
@@ -1,1 +1,5 @@
-export const isCreative = !!Deno.env.get('CREATIVE_MODE')
+import "dotenv"
+import toBoolean from "bool"
+
+// export const isCreative = !!Deno.env.get('CREATIVE_MODE')
+export const isCreative = toBoolean(Deno.env.get('CREATIVE_MODE'))
